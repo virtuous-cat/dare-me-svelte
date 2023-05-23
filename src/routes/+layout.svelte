d215873b75ef108b74d060e82089cac793cd7d51
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-05-23 07:07:13 -0700

    Add fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import "@fontsource/modak";
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    font-family: Seravek, "Gill Sans Nova", Ubuntu, Calibri, "DejaVu Sans",
+      source-sans-pro, sans-serif;
+  }
+  :global(.logo-font) {
+    font-family: "Modak", Seravek, "Gill Sans Nova", Ubuntu, Calibri,
+      "DejaVu Sans", source-sans-pro, sans-serif;
+  }
+</style>
