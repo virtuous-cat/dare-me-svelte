e45db1e994595bfeaf91bb6a4a77363927eedc5b
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-06-26 01:17:11 -0700

    After Prettier pass

--- websocketserver/index.ts
+++ websocketserver/index.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 
-import { Server } from 'socket.io'
-import { createServer } from 'http'
+import { Server } from "socket.io";
+import { createServer } from "http";
 import express from "express";
-import { handler } from '../build/handler.js'
+import { handler } from "../build/handler.js";
 import {
   type ClientToServerEvents,
   type InterServerEvents,
@@ -16,71 +16,71 @@ import {
 
 process.env.REDIS_URL;
 
-const port = 3000
-const app = express()
-const server = createServer(app)
+const port = 3000;
+const app = express();
+const server = createServer(app);
 
 const io = new Server<
-      ClientToServerEvents,
-      ServerToClientEvents,
-      InterServerEvents,
-      SocketData
-    >(server);
+  ClientToServerEvents,
+  ServerToClientEvents,
+  InterServerEvents,
+  SocketData
+>(server);
 
-    io.use((socket, next) => {
-      const roomResult = GameCodeValidator.safeParse(
-        socket.handshake.auth.gameRoom
-      );
-      console.log(roomResult);
-      const idResult = PlayerIdSchema.safeParse(socket.handshake.auth.playerId);
-      console.log(idResult);
-      const nameResult = PlayerNameValidator.safeParse(
-        socket.handshake.auth.playerName
-      );
-      console.log(nameResult);
-      if (!roomResult.success) {
-        console.log(roomResult.error);
-        return next(new Error("Invalid auth"));
-      }
-      if (!idResult.success) {
-        console.log(idResult.error);
-        return next(new Error("Invalid auth"));
-      }
-      if (!nameResult.success) {
-        console.log(nameResult.error);
-        return next(new Error("Invalid auth"));
-      }
-      socket.data.gameRoom = roomResult.data;
-      socket.data.playerId = idResult.data;
-      socket.data.playerName = nameResult.data;
-      next();
+io.use((socket, next) => {
+  const roomResult = GameCodeValidator.safeParse(
+    socket.handshake.auth.gameRoom
+  );
+  console.log(roomResult);
+  const idResult = PlayerIdSchema.safeParse(socket.handshake.auth.playerId);
+  console.log(idResult);
+  const nameResult = PlayerNameValidator.safeParse(
+    socket.handshake.auth.playerName
+  );
+  console.log(nameResult);
+  if (!roomResult.success) {
+    console.log(roomResult.error);
+    return next(new Error("Invalid auth"));
+  }
+  if (!idResult.success) {
+    console.log(idResult.error);
+    return next(new Error("Invalid auth"));
+  }
+  if (!nameResult.success) {
+    console.log(nameResult.error);
+    return next(new Error("Invalid auth"));
+  }
+  socket.data.gameRoom = roomResult.data;
+  socket.data.playerId = idResult.data;
+  socket.data.playerName = nameResult.data;
+  next();
+});
+
+io.on("connection", (socket) => {
+  const gameRoom = socket.data.gameRoom;
+  const playerId = socket.data.playerId;
+  const playerName = socket.data.playerName;
+
+  if (!gameRoom || !playerId || !playerName) {
+    socket.disconnect(true);
+    return;
+  }
+
+  socket.join([gameRoom, playerId]);
+
+  socket.emit("syncGameState", { players: [] });
+
+  socket.on("chat", (message) => {
+    socket.to(gameRoom).emit("serverChat", {
+      message,
+      playerId,
+      playerName,
     });
-
-    io.on("connection", (socket) => {
-      const gameRoom = socket.data.gameRoom;
-      const playerId = socket.data.playerId;
-      const playerName = socket.data.playerName;
-
-      if (!gameRoom || !playerId || !playerName) {
-        socket.disconnect(true);
-        return;
-      }
-
-      socket.join([gameRoom, playerId]);
-
-      socket.emit("syncGameState", { players: [] });
-
-      socket.on("chat", (message) => {
-        socket.to(gameRoom).emit("serverChat", {
-          message,
-          playerId,
-          playerName,
-        });
-      });
-    });
+  });
+});
 
 // SvelteKit should handle everything else using Express middleware
 // https://github.com/sveltejs/kit/tree/master/packages/adapter-node#custom-server
-app.use(handler)
+app.use(handler);
 
-server.listen(port)
+server.listen(port);
