b04b9a9af160e958d7a467b9ef85d33fa0279e46
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-07-26 08:34:20 -0700

    Style Modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -9,5 +9,18 @@
 <style>
   dialog {
     background-color: var(--background-color);
+    margin: auto 0.5rem;
+    padding-block: 1.5rem;
+    width: calc(100% - 1rem);
+    max-width: 1000px;
+    border-radius: var(--border-radius-large);
+    border: 1px solid var(--accent-color);
+    box-shadow: 0px 0px 20px 0px var(--accent-color) inset;
+    &::backdrop {
+      background-color: hsla(216, 37%, 8%, 0.4);
+    }
+    & > * {
+      margin-block-end: 1rem;
+    }
   }
 </style>
