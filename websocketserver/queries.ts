44691ff1f36284e0b7c916c2f6ff8b4cee2c40a3
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-07-26 05:14:17 -0700

    Test server redis in prod

--- websocketserver/queries.ts
+++ websocketserver/queries.ts
@@ -1,14 +1,14 @@
-// import Redis from "ioredis";
+import Redis from "ioredis";
 
-// const url = process.env.REDIS_URL;
+const url = process.env.REDIS_URL;
 
-// if (!url) {
-//   throw new Error("No REDIS_URL from env");
-// }
+if (!url) {
+  throw new Error("No REDIS_URL from env");
+}
 
-// const redis = new Redis(url);
+const redis = new Redis(url);
 
-// redis.on("error", function (error) {
-//   console.log("error from server redis");
-//   console.dir(error);
-// });
+redis.on("error", function (error) {
+  console.log("error from server redis");
+  console.dir(error);
+});
