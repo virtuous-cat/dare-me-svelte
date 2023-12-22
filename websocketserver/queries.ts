4b47d3f5cd18596c3399cb3a41b5344fef4896f3
VirtuousCat <91882409+virtuous-cat@users.noreply.github.com>
2023-12-22 08:32:58 -0800

    Outline redis keys

--- websocketserver/queries.ts
+++ websocketserver/queries.ts
@@ -12,3 +12,24 @@ redis.on("error", function (error) {
   console.log("error from server redis");
   console.dir(error);
 });
+
+// Game State
+// game:[code] Hash { turn, hostId, interaction, categories }
+
+// Added Dares
+// game:[code]:dares Set of dareIds
+
+// Players
+// game:[code]:players Set of playerIds
+
+// Player entries
+// game:[code]:player:[playerId] Hash { playerName, playerId, dares(stringified) }
+
+// Players daree turns
+// game:[code]:turns Sorted Set member = playerId, score = # of daree turns player has had
+
+// Players ready status
+// game:[code]:ready Sorted Set member = playerId, score = 1 if ready, 0 if not ready
+
+// Kicked Players
+// game:[code]:kicked Set of playerIds
